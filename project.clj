4b4112b8abfcea68c404fb775a49dd8ae21b4696
Sebastián Moreno <smoreno.uy@gmail.com>
2019-01-11 09:47:33 -0300

    updgrade version clojure to 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [metrics-clojure "2.7.0"]
-                 [metrics-clojure-jvm "2.7.0"]
-                 [metrics-clojure-graphite "2.7.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [metrics-clojure "2.7.0" :exclusions [org.clojure/clojure]]
+                 [metrics-clojure-jvm "2.7.0" :exclusions [org.clojure/clojure]]
+                 [metrics-clojure-graphite "2.7.0" :exclusions [org.clojure/clojure]]])
